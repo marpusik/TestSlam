8fda7b82d5ebc7846f580b6b367daa73444a466a
marpusik <marpusik1987@gmail.com>
2021-07-27 09:54:05 +0300

    plus

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function () {
+    $('.reviews-slider').slick({
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        nextArrow: '<button type="button" class="slick-arrow slick-prev"></button>',
+        prevArrow: '<button type="button" class="slick-arrow slick-next"></button>'
+    });
+});
